1def9cc4491be64fc9bf35f30a69c51baf83a967
ilias <ilias@linux.fritz.box>
2020-08-06 18:22:35 +0200

    added mask detection to video and starting phases of live feed detection

--- data_training.py
+++ data_training.py
@@ -2,13 +2,10 @@ from pathlib import Path
 
 import pandas as pd
 import pytorch_lightning as pl
+
 import torch
 import torch.nn.init as init
-from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
-from torch import Tensor
-from torch.nn import (Conv2d, CrossEntropyLoss, Linear, MaxPool2d, ReLU,
-                      Sequential)
+from torch import Tensor, nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
@@ -16,70 +13,66 @@ from data_class import MaskClass
 
 
 class MaskDetect(pl.LightningModule):
-    def __init__(self, maskDFPath: Path = None):
+    def __init__(self, maskPath:Path=None):
         super(MaskDetect, self).__init__()
-        self.maskDFPath = maskDFPath
+        self.maskPath = maskPath
 
-        self.maskDF = None
-        self.trainDF = None
-        self.validateDF = None
+        self.mask_dataframe = None
+        self.train_dataframe = None
         self.crossEntropyLoss = None
-        self.learningRate = 0.00001
 
-        self.convLayer1 = convLayer1 = Sequential(
-            Conv2d(3, 32, kernel_size=(3, 3), padding=(1, 1)),
-            ReLU(),
-            MaxPool2d(kernel_size=(2, 2))
+        self.convolution1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=(3, 3), padding=(1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2))
         )
 
-        self.convLayer2 = convLayer2 = Sequential(
-            Conv2d(32, 64, kernel_size=(3, 3), padding=(1, 1)),
-            ReLU(),
-            MaxPool2d(kernel_size=(2, 2))
+        self.convolution2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=(3, 3), padding=(1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2))
         )
 
-        self.convLayer3 = convLayer3 = Sequential(
-            Conv2d(64, 128, kernel_size=(3, 3), padding=(1, 1), stride=(3, 3)),
-            ReLU(),
-            MaxPool2d(kernel_size=(2, 2))
+        self.convolution3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=(3, 3), padding=(1, 1), stride=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2))
         )
 
-        self.linearLayers = linearLayers = Sequential(
-            Linear(in_features=2048, out_features=1024),
-            ReLU(),
-            Linear(in_features=1024, out_features=2),
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=2048, out_features=1024),
+            nn.ReLU(),
+            nn.Linear(in_features=1024, out_features=2),
         )
 
-        for sequential in [convLayer1, convLayer2, convLayer3, linearLayers]:
+        for sequential in [self.convolution1, self.convolution2, self.convolution3, self.linear]:
             for layer in sequential.children():
-                if isinstance(layer, (Linear, Conv2d)):
+                if isinstance(layer, (nn.Linear, nn.Conv2d)):
                     init.xavier_uniform_(layer.weight)
 
     def forward(self, x: Tensor):
-        """ forward pass
-        """
-        out = self.convLayer1(x)
-        out = self.convLayer2(out)
-        out = self.convLayer3(out)
+        out = self.convolution1(x)
+        out = self.convolution2(out)
+        out = self.convolution3(out)
         out = out.view(-1, 2048)
-        out = self.linearLayers(out)
+        out = self.linear(out)
         return out
 
     def prepare_data(self):
-        self.maskDF = maskDF = pd.read_pickle(self.maskDFPath)
-        self.trainDF = MaskClass(self.maskDF)
+        self.mask_dataframe = pd.read_pickle(self.maskPath)
+        self.train_dataframe = MaskClass(self.mask_dataframe)
 
-        maskNum = maskDF[maskDF['mask'] == 1].shape[0]
-        nonMaskNum = maskDF[maskDF['mask'] == 0].shape[0]
-        nSamples = [nonMaskNum, maskNum]
-        normedWeights = [1 - (x / sum(nSamples)) for x in nSamples]
-        self.crossEntropyLoss = CrossEntropyLoss(weight=torch.tensor(normedWeights))
+        mask_pictures = self.mask_dataframe[self.mask_dataframe['mask'] == 1].shape[0]
+        nonmask_pictures = self.mask_dataframe[self.mask_dataframe['mask'] == 0].shape[0]
+        number_samples = [nonmask_pictures, mask_pictures]
+        normalized = [1 - (x / sum(number_samples)) for x in number_samples]
+        self.crossEntropyLoss = nn.CrossEntropyLoss(weight=torch.tensor(normalized))
 
     def train_dataloader(self):
-        return DataLoader(self.trainDF, batch_size=32, shuffle=True, num_workers=4)
+        return DataLoader(self.train_dataframe, batch_size=32, shuffle=True, num_workers=4)
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=self.learningRate)
+        return Adam(self.parameters(), lr=1e-5)
 
     def training_step(self, batch: dict, _batch_idx: int):
         inputs, labels = batch['image'], batch['mask']
@@ -92,8 +85,7 @@ class MaskDetect(pl.LightningModule):
 if __name__ == '__main__':
     model = MaskDetect(Path('mask_df.pickle'))
 
-    trainer = Trainer(gpus=1 if torch.cuda.is_available() else 0,
-                      max_epochs=1,
-                      default_root_dir='weights.ckpt',
+    trainer = pl.Trainer(gpus=1 if torch.cuda.is_available() else 0,
+                      max_epochs=3,
                       profiler=True)
     trainer.fit(model)
